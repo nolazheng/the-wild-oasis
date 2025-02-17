37fda6ad5620b4cc8cb25768f001632b8f13f9e7
nolazheng <mania001988@gmail.com>
2025-02-17 17:55:37 +0800

    feat(middleware): redirect if user not login

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,27 @@
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+
 import { auth } from './app/_lib/auth';
 
+export async function middleware(req: NextRequest) {
+  const session = await auth();
+  const { pathname } = req.nextUrl;
+
+  if (pathname === '/login' && session?.user) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  if (pathname.startsWith('/account') && !session?.user) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  if (pathname === '/cabins/thankyou' && !session?.user) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  return NextResponse.next();
+}
+
 export const config = {
-  matcher: ['/account'],
+  matcher: ['/login', '/account/:path*', '/cabins/thankyou'],
 };
-
-// export function middleware(req: Request, res: Response) {
-//   return NextResponse.redirect(new URL('/about', req.url));
-// }
-
-export const middleware = auth;
